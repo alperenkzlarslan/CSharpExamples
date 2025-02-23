91bcc2279283d656cc0b3c49b98f4a2104c67b23
Alperen Kızılarslan <alperenkizilarslann@gmail.com>
2025-02-23 13:56:33 +0300

    ilk örnekler eklendi ve .gitignore dosyası eklendi

---
+++ MyProject/Program.cs
@@ -0,0 +1,17 @@
+﻿
+//Text examples 
+Console.WriteLine("Hello, World!");
+Console.Write("Alperen Kızılarslan");
+Console.WriteLine(" Hoş Geldin");
+
+//Number examples 
+Console.WriteLine(5 + 5);
+Console.WriteLine(10 / 2);
+
+//Comments examples
+//This is a comment line 
+Console.WriteLine("Merhaba Dünya"); //Çift slash den sonrası yorum satırı 
+
+
+Console.ReadKey();
+
